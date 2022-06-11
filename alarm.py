bb30091bdfb9fdbd72679a3cb49fd807c2e196ae
djon125 <ross.dion9027@gmail.com>
2022-06-11 10:52:35 -0500

    added toggle and set alarm

--- alarm.py
+++ alarm.py
@@ -2,6 +2,21 @@
 class AlarmClock:
     def __init__(self):
         self.current_time = '2:30'
-        self.alarm_on = True
         self.alarm_time = '6:30'
+        self.alarm_status = ''
 
+    def set_current_time(self):
+        self.current_time = input('Time: ')
+        print(self.current_time)
+
+#toggle alarm
+    def toggle_alarm(self, alarm_toggle):
+        if self.alarm_status == True:
+            print('Alarm is active')
+        else:
+            print('Alarm is off')
+
+#set alarm time
+    def set_alarm_time(self):
+        self.alarm_time = input('Time: ')
+        print(self.alarm_time)
--- main.py
+++ main.py
@@ -2,5 +2,7 @@ from alarm import AlarmClock
 
 
 DionAlarm =AlarmClock()
-print(DionAlarm.alarm_on)
 print(DionAlarm.current_time)
+#DionAlarm.set_current_time()
+DionAlarm.toggle_alarm(False)
+DionAlarm.set_alarm_time()
