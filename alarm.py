7380943c9ac32b6591814b8add722b23faedb79c
djon125 <ross.dion9027@gmail.com>
2022-06-11 10:00:30 -0500

    created alarm class

---
+++ alarm.py
@@ -0,0 +1,7 @@
+
+class AlarmClock:
+    def __init__(self):
+        self.current_time = '2:30'
+        self.alarm_on = True
+        self.alarm_time = '6:30'
+
---
+++ main.py
@@ -0,0 +1,6 @@
+from alarm import AlarmClock
+
+
+DionAlarm =AlarmClock()
+print(DionAlarm.alarm_on)
+print(DionAlarm.current_time)
