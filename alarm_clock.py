19f101ecde2d286a143cd5585363d64a889118c5
djon125 <ross.dion9027@gmail.com>
2022-06-12 14:15:11 -0500

    fixed alarm toggle bug

--- alarm_clock.py
+++ alarm_clock.py
@@ -3,15 +3,17 @@ class AlarmClock:
     def __init__(self):
         self.current_time = '2:30'
         self.alarm_time = '6:30'
-        self.alarm_status = ''
+        self.alarm_status = True
+        self.alarm_toggle = ''
 
     def set_current_time(self):
         self.current_time = input('Time: ')
         print(self.current_time)
 
 #toggle alarm
-    def toggle_alarm(self, alarm_toggle):
-        if self.alarm_status == True:
+    def toggle_alarm(self, alarm_toggle_pass_in):
+        self.alarm_toggle = alarm_toggle_pass_in
+        if self.alarm_toggle == True:
             print('Alarm is active')
         else:
             print('Alarm is off')
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from alarm import AlarmClock
+from alarm_clock import AlarmClock
 
 
 DionAlarm =AlarmClock()
 print(DionAlarm.current_time)
-#DionAlarm.set_current_time()
+DionAlarm.toggle_alarm(True)
 DionAlarm.toggle_alarm(False)
-DionAlarm.set_alarm_time()
+DionAlarm.toggle_alarm(True)
